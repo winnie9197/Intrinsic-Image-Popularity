3250004e10226df4bfb4fdcf0946b1c6fb8c77db
winniehcy <winniehcyeung@gmail.com>
2023-05-05 00:17:50 -0400

    Modify prediction function to get and sort image scores by URL

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 import argparse
+import urllib.request
 import torch
 import torchvision.models
 import torchvision.transforms as transforms
+from collections import OrderedDict
 from PIL import Image
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 def prepare_image(image):
+    # TODO: optimize function for faster URL -> rgb conversion
     if image.mode != 'RGB':
         image = image.convert("RGB")
     Transform = transforms.Compose([
@@ -21,7 +24,32 @@ def predict(image, model):
     image = prepare_image(image)
     with torch.no_grad():
         preds = model(image)
-    print(r'Popularity score: %.2f' % preds.item())
+
+    return preds.item()
+
+## The function should return a list of predicted image scores, along with image urls/ids
+def get_image_scores(urls):
+    # Prepare Model
+    model = torchvision.models.resnet50()
+    # model.avgpool = nn.AdaptiveAvgPool2d(1) # for any size of the input
+    model.fc = torch.nn.Linear(in_features=2048, out_features=1)
+    model.load_state_dict(torch.load('intrinsic_image_popularity/model/model-resnet50.pth', map_location=device)) 
+    model.eval().to(device)
+
+    scores = OrderedDict()
+    for url in urls:
+        urllib.request.urlretrieve(url, 'downloaded_image.jpg')
+        image = Image.open('downloaded_image.jpg')
+        model.eval().to(device)
+        prediction = predict(image, model)
+        scores[url] = prediction
+    return sort_image_scores(scores)
+
+def sort_image_scores(scores):
+    # Sorted Descending. A higher IIPA value indicates better intrinsic popularity (see IIPA paper)
+    sorted_scores = sorted(scores.items(), key=lambda x:x[1], reverse=True)
+    return sorted_scores
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -31,6 +59,6 @@ if __name__ == '__main__':
     model = torchvision.models.resnet50()
     # model.avgpool = nn.AdaptiveAvgPool2d(1) # for any size of the input
     model.fc = torch.nn.Linear(in_features=2048, out_features=1)
-    model.load_state_dict(torch.load('model/model-resnet50.pth', map_location=device)) 
+    model.load_state_dict(torch.load('intrinsic_image_popularity/model/model-resnet50.pth', map_location=device)) 
     model.eval().to(device)
     predict(image, model)
